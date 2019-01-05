5dce47968998989dfaa451a846cb59338a454701
Viacheslav Haidei <haideigv@gmail.com>
2019-01-05 22:52:29 +0200

    Fixed Get method

--- src/number_handler.erl
+++ src/number_handler.erl
@@ -4,10 +4,9 @@
 -export([init/2]).
 
 init(Req0=#{method := <<"GET">>}, State) ->
-	io:fwrite(">> POST REQUEST >> ~n~w~n", Req0),
 	Req = cowboy_req:reply(200,
 		#{<<"content-type">> => <<"text/plain">>},
-		<<100, 101, 102, 103>>,
+		<<"99999999999999">>,
 		Req0
 	),
 	{ok, Req, State};
@@ -16,15 +15,15 @@ init(Req0=#{method := <<"POST">>}, State) ->
 	{ok, Data, _} = cowboy_req:read_body(Req0),
 	{DataResult} = jiffy:decode(Data),
 	{_, Value} = lists:last(DataResult),
-	io:fwrite(">> IS INT >> ~n~w~n", [erlang:is_integer(Value)]),
-	io:fwrite(">> IS BINARY >> ~n~w~n", [erlang:is_binary(Value)]),
 	Result = erlang:integer_to_binary(Value * Value),
-	io:fwrite(">> REQUEST >> ~n~w~n", [Result]),
-	Req = cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, Result, Req0),
+	Req = cowboy_req:reply(200,
+		#{<<"content-type">> => <<"text/plain">>}, 
+		Result, 
+		Req0
+	),
 	{ok, Req, State};
 
 init(Req0=#{method := <<_>>}, State) ->
-	io:fwrite(">> OTHER REQUEST >> ~n~w~n", [Req0]),
 	Req = cowboy_req:reply(405,
 		#{<<"content-type">> => <<"text/plain">>},
 		<<"Method not alowed">>,
