75d8f1f133e46481c84350203355274432356356
Viacheslav Haidei <haideigv@gmail.com>
2019-01-05 22:44:33 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,10 @@
+PROJECT = webserver
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+
+DEPS = cowboy jiffy
+dep_cowboy_commit = 2.6.1
+
+DEP_PLUGINS = cowboy
+
+include erlang.mk
---
+++ src/webserver_app.erl
@@ -0,0 +1,22 @@
+-module(webserver_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/", hello_handler, []}, 
+			{"/num", number_handler, []}
+		]}
+	]),
+	{ok, _} = cowboy:start_clear(
+		my_http_listener,
+		[{port, 8080}],
+		#{env => #{dispatch => Dispatch}}
+	),
+	webserver_sup:start_link().
+
+stop(_State) ->
+	ok.
